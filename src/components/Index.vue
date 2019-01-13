208d5dbc9b1646e52e7a4cd3b97c952c4e8e526a
Adidan Tsai <bill3129066@gmail.com>
2019-01-13 17:28:45 +0800

    finished main page

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,73 +1,50 @@
 <template>
-  <div class="index container">
-    <!-- <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
-      <div class="card-content">
-        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
-        <h2 class="indego-text">{{ smoothie.title }}</h2>
-        <ul class="ingredients">
-          <li v-for="(ing, index) in smoothie.ingredients" :key="index">
-            <span class="chip">{{ ing }}</span>
-          </li>
-        </ul>
-      </div>
-    </div>-->
-    <div class="row align-items-center">
-      <div class="col-6 mx-auto col-md-6 order-md-2">
-		<p><img
-          alt="Logo"
-          class="img-fluid mx-auto d-block mb-3 mb-md-0"
-          src="https://www.ntu.edu.tw/images/inner/emblem_1.png"
-		  align="left"
-        >做出我們應盡的心力。大學是人生中至關重要的一個選擇，它深深影響了大多數人的人生軌跡，小至大學生涯的生活環境、大至未來後半</p>
-      </div>
-      <div class="col-md-6 order-md-1 text-left text-md-left pr-md-5">
-        <h1 class="mb-3">新世代大學科系資訊系統</h1>
-        <p
-          class="lead"
-         
-        >大學是人生中至關重要的一個選擇，它深深影響了大多數人的人生軌跡，小至大學生涯的生活環境、大至未來後半生的工作都有可能因為我們在 18 歲時的那個選擇而有著大大的不同。</p>
-        <p class="lead mb-3">
-          但一件這麼重要的事情卻常常不受到重視，導致高中生最終最初了一個讓自己後悔的決定，而我們在看到了許多沈重的例子後決定站出來，改變這個現狀。
+    <div class="main-page container">
+        <h2>《科系系》- 新世代大學科系資訊提供者</h2>
+        <p>
+            <h4>故事起源</h4>
+            大學是人生中至關重要的一個選擇，它深深影響了大多數人的人生軌跡，小至大學生涯的生活環境、大至未來後半生的工作都有可能因為我們在 18 歲時的那個選擇而有著大大的不同。<br>
+            但一件這麼重要的事情卻常常不受到重視，導致高中生最終最初了一個讓自己後悔的決定，而我們在看到了許多沈重的例子後決定站出來，改變這個現狀。
         </p>
-        <p class="mb-2 text-muted">
-          <a href="https://getbootstrap.com/docs/4.0">新世代大學索引</a> is the world's most popular framework for searching university and its relatives' sites.
+        <p>
+            <h4>現行制度哪裡出了問題？</h4>
+            我們研究發現在高中三年中，大多數高中生都集中在高三下考完試後的幾個星期才展開生涯探索，其他幾乎所有的時間都拿來學習學校的課業以及經營課外活動。<br>
+            但是要在這短短的幾週內就找到適合自己得科系絕非一件易事，曾經就有一份報導說到近四成大學生都希望選讀與現在不同的科系。<br>
+            根據我們的調查發現學校的生涯規劃課程因為時間不夠充足，大多採取廣而不深的策略，讓學生能對於不同類組有概略的瞭解，但事實上各類組下面個科系相差甚鉅，科系名稱相差一個字可能課程內容與未來出路就差了十萬八千里，而市面上紙本以及線上科系資料多以受訪者視角出發，導致過於片面且冗長，網路上的官方資料也多以宣傳系所的角度出發，會有隱惡揚善的問題。
         </p>
-      </div>
+        <p>
+            <h4>解決方式</h4>
+            我們透過訪談極端使用者（重考生、轉系生以及對現在科系感到不滿的人）從他們的視角找出了他們在選擇大學科系時重視的資訊，並至做出科系百科的
+            prototype，並透過實際進班做使用者訪談修正內容綱要，期待整理出一份真正對高中生有幫助的科系百科。
+        </p>
+
+        <div class="photos">
+            <img class="div1" src="./assets/img1.jpg">
+            <img class="div2" src="./assets/img2.jpg">
+        </div>
+
     </div>
-  </div>
 </template>
 
 <script>
-import db from "@/firebase/init";
-export default {
-};
+    import db from "@/firebase/init";
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.index {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 30px;
-  margin-top: 60px;
+.photos{
+	display:flex;
+  
 }
-
-.index h2 {
-  font-size: 1.8em;
-  text-align: center;
-  margin-top: 0px;
+.div1{
+	width:50%;
+  height:250px;
+	flex:1;
+  padding: 5px;
 }
-
-.index .ingredients li {
-  display: inline-block;
-}
-
-.index .delete {
-  position: absolute;
-  top: 4px;
-  right: 4px;
-  cursor: pointer;
-  color: #aaa;
-  font-size: 1.4em;
+.div2{
+	width:50%;
+	height:250px;
+	flex:1;
+  padding: 5px;
 }
 </style>
