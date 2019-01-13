fb881679d5437edd426c386575360c969bbd6ceb
B05705055 <46625261+B05705055@users.noreply.github.com>
2019-01-13 11:30:36 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
     <nav class="nav-extended indigo darken-2 z-depth-3">
       <div class="nav-content">
-        <router-link to>
+        <router-link to="/">
           <span class="nav-title">Database</span>
         </router-link>
         <!-- <a href class="btn-floating btn-large halfway-fab pink">
